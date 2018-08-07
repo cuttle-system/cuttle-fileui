1d840ad8e8d8dc1dc142e6cfc40a2d3f3fe6c8c4
Michael Vietluzhskykh <mishavetl@gmail.com>
2018-08-07 12:37:29 +0300

    add massive-commit.sh

---
+++ massive-commit.sh
@@ -0,0 +1,30 @@
+function commit() {
+  git checkout master
+  git submodule update --remote --recursive
+  git add --all
+  git commit
+  git pull
+  git push origin master
+}
+
+cd tests/cuttle-test
+commit
+cd ../..
+
+cd deps/cuttle-parser
+commit
+cd ../..
+
+cd deps/cuttle-translator
+commit
+cd ../..
+
+cd deps/cuttle-generator
+commit
+cd ../..
+
+cd deps/cuttle-vm
+commit
+cd ../..
+
+commit
