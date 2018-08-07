5ac412397def322f48488ddbd2299414a5c65d42
Michael Vietluzhskykh <mishavetl@gmail.com>
2018-08-07 12:20:03 +0300

    update deps, gitignore and modules

--- build_deps.sh
+++ build_deps.sh
@@ -1,5 +1,5 @@
 git clone --recursive git@github.com:boostorg/boost.git deps/boost
 cd deps/boost
-./bootstrap gcc
+./bootstrap.sh gcc
 ./b2 --with-filesystem --build-type=minimal variant=release --toolset=gcc address-model=64 stage --layout=system
 cd ../..
