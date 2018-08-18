45b7162d34456ec99cfb4d8997abb253ce8c9436
Michael Vietluzhskykh <mishavetl@gmail.com>
2018-08-18 14:32:01 +0300

    finally setup new dependency manager

--- scripts/get-deps.sh
+++ scripts/get-deps.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd $(dirname "$0")/../..
 
+WORKSPACE_PATH=$(pwd)
+
 git clone git@github.com:boostorg/boost.git
 cd boost
 git checkout boost-1.65.1
@@ -8,27 +10,31 @@ git submodule update --init
 ./bootstrap.sh gcc
 ./b2 --with-filesystem --build-type=minimal variant=release --toolset=gcc address-model=64 stage --layout=system 
 
-cd $(dirname "$0")/../..
+cd $WORKSPACE_PATH
 
 git clone git@github.com:cuttle-system/cuttle-parser.git
 cuttle-parser/scripts/get-deps.sh
 
-cd $(dirname "$0")/../..
+cd $WORKSPACE_PATH
 
 git clone git@github.com:cuttle-system/cuttle-translator.git
 cuttle-translator/scripts/get-deps.sh
 
-cd $(dirname "$0")/../..
+cd $WORKSPACE_PATH
 
 git clone git@github.com:cuttle-system/cuttle-generator.git
 cuttle-generator/scripts/get-deps.sh
 
-cd $(dirname "$0")/../..
+cd $WORKSPACE_PATH
 
 git clone git@github.com:cuttle-system/cuttle-vm.git
 cuttle-vm/scripts/get-deps.sh
 
-cd $(dirname "$0")/../..
+cd $WORKSPACE_PATH
 
 git clone git@github.com:cuttle-system/cuttle-lang.git
 cuttle-lang/scripts/get-deps.sh
+
+cd $WORKSPACE_PATH
+
+git clone git@github.com:cuttle-system/cuttle-test.git
