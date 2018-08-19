b5d34fb9f2411fa9adbde79f05acb7893f427472
Michael Vietluzhskykh <mishavetl@gmail.com>
2018-08-19 14:23:56 +0300

    update dependency management

--- scripts/get-deps.sh
+++ scripts/get-deps.sh
@@ -1,36 +1,11 @@
-#!/bin/bash
-cd $(dirname "$0")/../..
+#!/usr/bin/env bash
 
-WORKSPACE_PATH=$(pwd)
+cd $(dirname "$0")
+SCRIPTS_PATH=$(pwd)
+cd ../..
 
-git clone git@github.com:boostorg/boost.git
-cd boost
-git checkout boost-1.65.1
-git submodule update --init
-./bootstrap.sh gcc
-./b2 --with-filesystem --with-test --build-type=minimal variant=release --toolset=gcc address-model=64 stage --layout=system
-
-cd $WORKSPACE_PATH
-
-git clone git@github.com:cuttle-system/cuttle-parser.git
-cuttle-parser/scripts/get-deps.sh
-
-cd $WORKSPACE_PATH
-
-git clone git@github.com:cuttle-system/cuttle-translator.git
-cuttle-translator/scripts/get-deps.sh
-
-cd $WORKSPACE_PATH
-
-git clone git@github.com:cuttle-system/cuttle-generator.git
-cuttle-generator/scripts/get-deps.sh
-
-cd $WORKSPACE_PATH
-
-git clone git@github.com:cuttle-system/cuttle-vm.git
-cuttle-vm/scripts/get-deps.sh
-
-cd $WORKSPACE_PATH
-
-git clone git@github.com:cuttle-system/cuttle-lang.git
-cuttle-lang/scripts/get-deps.sh
+for dep in $SCRIPTS_PATH/deps/*
+do
+    echo Getting dependency: $dep
+    $dep
+done
