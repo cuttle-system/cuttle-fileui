0a17d254598cc824b7869b4abdab3b7c8454e705
Michael Vietluzhskykh <mishavetl@gmail.com>
2018-10-16 21:13:48 +0300

    add support for empty file cache

--- src/lib/file_interpreter.cpp
+++ src/lib/file_interpreter.cpp
@@ -8,6 +8,9 @@ namespace fs = boost::filesystem;
 void fileui::interpret_file(vm::context_t &context, const fs::path &file_path, std::deque<vm::value_t> &arg_stack) {
     std::ifstream config_file(file_path.string());
     while (!config_file.eof()) {
+        if (config_file.peek() == EOF) {
+            break;
+        }
         vm::eval(config_file, context, arg_stack);
     }
     config_file.close();
